f7d965fd9e64c46d6b86eae41e02401d84e911a5
Akuot-philip <akuotphilipp@gmail.com>
2022-06-11 09:39:17 +0300

    Added bottom navigation

--- app/src/main/java/dev/akuot/workoutlog/LoginActivity.kt
+++ app/src/main/java/dev/akuot/workoutlog/LoginActivity.kt
@@ -42,12 +42,20 @@ class LoginActivity : AppCompatActivity() {
     private fun validateLogin(){
       val email=etEmail.text.toString()
         val password=etPassword.text.toString()
+        var error=false
 
         if (email.isBlank()){
             tilEmail.error =getString(R.string.email_required)
+            error =true
+
         }
         if (password.isBlank()){
             tilPassword.error ="Email is required"
+            error=true
+        }
+        if (error){
+            startActivity(Intent(this,HomeActivity::class.java))
+            finish()
         }
     }
 }
